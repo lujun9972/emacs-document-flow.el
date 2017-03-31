29eef4788e367a091b06bfcc97fd7312bc2069c0
darksun <lujun9972@gmail.com>
2017-03-31 16:23:31 +0800

    自动控制版本

--- emacs-document.el
+++ emacs-document.el
@@ -20,13 +20,17 @@
 
 输入url,然后转成$title.org放到raw目录下"
   (interactive)
-  (let* ((dom (html2org-get-dom))
+  (let* ((default-directory emacs-document-directory)
+         (dom (html2org-get-dom))
          (org-content (html2org-transform-dom dom))
          (org-title (progn (string-match "#\\+URL: \\([^\n]+\\)" org-content)
                            (match-string-no-properties 1 org-content)))
          (org-file (expand-file-name (concat org-title ".org") (emacs-document-raw-directory))))
     (with-temp-file org-file
-      (insert org-content))))
+      (insert org-content))
+    (vc-git-register (list org-file))
+    (vc-git-checkin (list org-file) (format "add raw post %s" org-title))
+    (vc-git-push nil)))
 
 (defun emacs-document-new-translation ()
   "开始新的翻译
@@ -34,12 +38,20 @@
 从raw目录中挑选一篇文章放到processing目录下,然后打开这篇文章开始翻译
 若raw目录中也没有待翻译的文章,调用`emacs-document-new' 新增一篇待翻译的文章,然后再调用`emacs-document-new-translation' 开始翻译"
   (interactive)
-  (let* ((raw-directory (emacs-document-raw-directory))
+  (let* ((default-directory emacs-document-directory)
+         (raw-directory (emacs-document-raw-directory))
          (raw-file (read-file-name "请选择要翻译的文章: " raw-directory nil t))
          (filename (file-name-nondirectory raw-file))
          (process-file (expand-file-name filename (emacs-document-processing-directory))))
-    (rename-file raw-file process-file)
+    (if (vc-git-registered raw-file)
+        (vc-git-rename-file raw-file process-file)
+      (rename-file raw-file process-file)
+      (vc-git-register (list process-file)))
+    (vc-git-checkin (list raw-file process-file) (format "processing post %s" filename))
+    (vc-git-push nil)
     (find-file process-file)))
+
+(vc-registered "~/github/emacs-document/raw/literate-devops.org")
 
 (defun emacs-document-start-translation ()
   "开始翻译
